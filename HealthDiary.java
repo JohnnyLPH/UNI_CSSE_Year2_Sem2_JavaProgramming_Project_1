a04163c5f1a041cd5e20ea78d262533304079b15
Lau, P. H <70358914+JohnnyLPH@users.noreply.github.com>
2022-04-16 11:53:42 +0800

    initial commit

---
+++ HealthDiary.java
@@ -0,0 +1,28 @@
+// TMF2954 Java Programming Group Project
+// Group Name: LUCKY
+// Group Members:
+//     - LAU PIKK HEANG (75359)
+//     - YUKI CHUNG PEI YING (77237)
+//     - ANDREA ANG XIAO XUAN (73347)
+// Health Diary App: Main Function is in this file.
+import javax.swing.JFrame;
+import java.awt.Dimension;
+
+
+public class HealthDiary {
+    public static void main(String[] args) {
+        // Frame for Health Diary app.
+        JFrame appFrame = new JFrame("Health Diary by LUCKY");
+        appFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        // Based on iPhone SE display resolution (750 * 1334).
+        appFrame.setPreferredSize(new Dimension(375, 667));
+
+        // TODO: Add content to app frame, first create other classes.
+
+        // Display app frame.
+        appFrame.pack();
+        appFrame.setVisible(true);
+        return;
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # UNI_CSSE_Year2_Sem2_JavaProgramming_Project_1
 For Year 2 Semester 2 Java Programming Project.
+
+    Preferred Text Editor:
+        VSCode
+
+    Other Recommended Tool(s):
+        GitHub Desktop
